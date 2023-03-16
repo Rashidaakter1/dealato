594097802a65a7e51a10cff0530841d644a05df5
Rashidaakter1 <rashidaakterchadni@gmail.com>
2023-03-16 14:42:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>this is header</h1>
       </header>
-    </div>
+      <main>
+        <h1>this is main</h1>
+      </main>
+      <footer>footer</footer>
+    </>
   );
 }
 
